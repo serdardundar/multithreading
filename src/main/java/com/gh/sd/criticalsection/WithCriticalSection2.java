0938a2b5a2aeda1982775318c122769fa72f550a
Serdar Dundar <serdar.dundar@comeon.com>
2024-09-19 23:35:53 +0300

    critical section implemented
    critical section implemented

---
+++ src/main/java/com/gh/sd/criticalsection/WithCriticalSection2.java
@@ -0,0 +1,78 @@
+package com.gh.sd.criticalsection;
+
+/**
+ * Now we have synchronized methods so the result never changes
+ * Synchronized methods are defined as Critical Section
+ */
+public class WithCriticalSection2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
+    private static class DecrementingThread extends Thread {
+
+        private final InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    private static class IncrementingThread extends Thread {
+
+        private final InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter) {
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10000; i++) {
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+    private static class InventoryCounter {
+        private int items = 0;
+        final Object lock = new Object();
+
+        public void increment() {
+            synchronized (lock) {
+                items++;
+            }
+        }
+
+        public void decrement() {
+            synchronized (lock) {
+                items--;
+            }
+
+        }
+
+        public int getItems() {
+            synchronized (lock) {
+                return items;
+            }
+        }
+    }
+}
